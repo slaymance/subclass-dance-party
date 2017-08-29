2d9b40c8d3713ff50204ab2771a3c8ac1ab56bd5
Shane Laymance <shane.laymance@gmail.com>
2017-08-28 20:03:35 -0700

    refactored using ES6 in the pseudoclassical pattern

--- src/init.js
+++ src/init.js
@@ -15,14 +15,17 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
+
+    const classesMapping = {
+      'makeBlinkyDancer': makeBlinkyDancer,
+    };
+
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
-
+    var dancerMakerFunction = classesMapping[dancerMakerFunctionName];
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new makeBlinkyDancer(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
