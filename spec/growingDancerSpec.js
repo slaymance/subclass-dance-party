546a3d9cfb61004bf72d684b4c082d2ab7a9336e
Shane Laymance <shane.laymance@gmail.com>
2017-08-28 21:40:30 -0700

    Add tests for growingDancer class

---
+++ spec/growingDancerSpec.js
@@ -0,0 +1,28 @@
+describe('growingDancer', function() {
+
+  var growingDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    growingDancer = new makeGrowingDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(growingDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  describe('grow', function() {
+    it('should switch classes between "growing" and "shrinking" each time step is called', function() {
+      sinon.spy(growingDancer, 'step');
+      
+      expect(growingDancer.$node.hasClass('growing')).to.be.true;
+      
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+      clock.tick(timeBetweenSteps);
+      
+      expect(growingDancer.$node.hasClass('shrinking')).to.be.true;
+    });
+  });
+});
